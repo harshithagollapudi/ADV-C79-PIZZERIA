1fbf8efcd1b46182552f97845216e64c6cf4b45d
gollapudiharshitha <79522870+harshithagollapudi@users.noreply.github.com>
2022-04-12 23:33:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-menu_list_array = ["Chicken Tandoori Pizza","Deluxe Veggie Pizza","Paneer Tikka Pizza","Veg Margherita Pizza","Veg Supreme Pizza"];
 
-function getmenu(){
-var htmldata;
-htmldata="<ol class='menulist'>"
-menu_list_array.sort();
-for(var i=0;
-i<menu_list_array.length;i++){ htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>' } htmldata=htmldata+"</ol>"
-document.getElementById("display_menu").innerHTML = htmldata;
+Status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(300, 290);
+    canvas.position(480, 250);
+    video = createCapture(VIDEO);
+    video.size(300, 290);
+    video.hide();
 }
-
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value; 
-menu_list_array.sort(); 
-htmldata="<section class='cards'>"; 
-for(var i=0;i<menu_list_array.length;i++){ htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>' } htmldata=htmldata+"</section>"
-document.getElementById("display_addedmenu").innerHTML = htmldata;
+function start(){
+    object_detector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input_text = document.getElementById("input_id").value;
 }
-
-function add_top(){
-    var item=doucument.getElementById("add_item").value;
-    menu_list_array.push(item);
-    add_item;
+function modelLoaded(){
+    console.log("modelLoaded");
+    Status = true;
+}
+function draw(){
+    image(video, 0, 0, 300, 290);
 }
